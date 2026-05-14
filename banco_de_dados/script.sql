5b08973619fd13f09907cb6c5661f5216762714d
lucasvrcc <lucasvalentim@soulasalle.com.br>
2026-05-14 17:18:27 -0300

    página inscrições + tabela sql

--- banco_de_dados/script.sql
+++ banco_de_dados/script.sql
@@ -1,1 +1,13 @@
+CREATE TABLE voluntarios (
 
+    id SERIAL PRIMARY KEY,
+
+    -- Campos obrigatórios
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+
+    -- Campos opcionais
+    telefone VARCHAR(20),
+
+    data_inscricao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
